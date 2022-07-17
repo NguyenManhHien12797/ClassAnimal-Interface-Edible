6d83799edf582b06890963d73ff5e0c0a5e5c2e7
NguyenManhHien12797 <nguyenhien81f@gmail.com>
2022-07-17 09:15:03 +0700

    class Animal and Interface Edible

---
+++ src/fruit/Orange.java
@@ -0,0 +1,10 @@
+package fruit;
+
+import sun.text.normalizer.CharTrie;
+
+public class Orange extends Fruit {
+    @Override
+    public String howToEat() {
+        return "Orange could be juiced";
+    }
+}
